a523a16c1f3220b330a946e60812d12fc1af7fc6
Calvin Li <calvinliqh@gmail.com>
2024-02-25 23:28:39 -0800

    added gallery click functionality

--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -1,6 +1,10 @@
 map_cover = document.getElementById("c2")
 hide_button = document.getElementById("HideMapButton")
+gallery_selected_img = document.getElementById("gallery_selected_img")
 populate_gallery()
+
+var gallery_selected_img_name = "eiffel.jpg"
+update_gallery_selected()
 
 function toggleMap(){
     toggle_fade(map_cover)
@@ -22,14 +26,27 @@ function toggle(element, t_class){
     }
 }
 
+function update_gallery_selected(img_name = gallery_selected_img_name){
+    gallery_selected_img.src = `media/gallery/${img_name}`
+}
+
 function populate_gallery(){
     selector = document.getElementById("gallery_selector")
 
     for(img_name of gallery_contents){
         const new_img = document.createElement("img")
         new_img.className = "gallery_thumbnail"
-        new_img.src = "media/gallery/" + img_name
-
+        new_img.src =  `media/gallery/${img_name}`
+        new_img.setAttribute("onclick", `select_gallery("${img_name}")`)
+        new_img.setAttribute(
+            "onmouseover", `update_gallery_selected("${img_name}")`)
+        new_img.setAttribute("onmouseout", "update_gallery_selected()")
+    
         selector.appendChild(new_img)
     }
+}
+
+function select_gallery(img_name){
+    gallery_selected_img_name = img_name
+    update_gallery_selected()
 }
